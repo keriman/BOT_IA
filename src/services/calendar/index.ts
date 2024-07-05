c5eb846f673e24f772b45e0006cb256bfe673b98
keriman <keriman@gmail.com>
2024-07-05 10:22:40 -0600

    first commit

---
+++ src/services/calendar/index.ts
@@ -0,0 +1,42 @@
+import { format, addMinutes } from 'date-fns'
+
+/**
+ * get calendar
+ * @returns 
+ */
+const getCurrentCalendar = async (): Promise<string> => {
+    const dataCalendarApi = await fetch('https://hook.us1.make.com/gsxekflz3j7llmifo5e4c5erfvgdl6sl')
+    const json: any[] = await dataCalendarApi.json()
+    const list = json.reduce((prev, current) => {
+        return prev += [
+            `Espacio reservado (no disponible): `,
+            `Desde ${format(current.date, 'eeee do h:mm a')} `,
+            `Hasta ${format(addMinutes(current.date, 45), 'eeee do h:mm a')} \n`,
+        ].join(' ')
+    }, '')
+    return list
+}
+
+/**
+ * add to calendar
+ * @param text 
+ * @returns 
+ */
+const appToCalendar = async (text: string) => {
+    try {
+        const payload = JSON.parse(text)
+        console.log(payload)
+        const dataApi = await fetch('https://hook.us1.make.com/ctlays2q75bx355d6ydw6x27nuv4j2md', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload)
+        })
+        return dataApi
+    } catch (err) {
+        console.log(`error: `, err)
+    }
+}
+
+export { getCurrentCalendar, appToCalendar }
